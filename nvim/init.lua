7b6b51675b16021de67e20823f5e528c05024192
Aaron Nguyen <37777232+aatng-gh@users.noreply.github.com>
2025-04-22 07:37:12 -0400

    Enhance!

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,7 @@
 -- [[ Options ]] {{{
+vim.g.nerd_font = true
+vim.g.border_style = 'single'
+
 vim.o.foldmethod = 'marker'
 vim.o.foldlevel = 99
 
@@ -36,7 +39,6 @@ add({
   source = 'nvim-treesitter/nvim-treesitter',
   hooks = { post_checkout = function() vim.cmd('TSUpdate') end },
 })
-
 -- }}}
 
 -- [[ UI ]] {{{
@@ -50,26 +52,14 @@ now(function()
 
   vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
   vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
-
-  require('mini.notify').setup()
-  vim.notify = require('mini.notify').make_notify({
-    ERROR = { duration = 5000 },
-    WARN = { duration = 4000 },
-    INFO = { duration = 3000 },
-  })
-
-  require('mini.basics').setup() -- TODO: read more
-  require('mini.indentscope').setup()
 end)
--- }}}
 
--- [[ LSP ]] {{{
 now(function()
   vim.diagnostic.config({
     severity_sort = true,
-    float = { border = 'single', source = 'if_many' },
+    float = { border = vim.g.border_style, source = 'if_many' },
     underline = { severity = vim.diagnostic.severity.ERROR },
-    signs = vim.g.have_nerd_font and {
+    signs = vim.g.nerd_font and {
       text = {
         [vim.diagnostic.severity.ERROR] = '󰅚 ',
         [vim.diagnostic.severity.WARN] = '󰀪 ',
@@ -91,8 +81,71 @@ now(function()
       end,
     },
   })
+end)
+-- }}}
 
-  vim.lsp.enable({ 'luals' }) -- should always be in now()
+-- [[ Workflows ]] {{{
+now(function()
+  require('mini.basics').setup() -- TODO: read more
+end)
+
+later(function()
+  require('mini.notify').setup()
+  vim.notify = require('mini.notify').make_notify({
+    ERROR = { duration = 5000 },
+    WARN = { duration = 4000 },
+    INFO = { duration = 3000 },
+  })
+end)
+
+later(function()
+  require('mini.indentscope').setup()
+end)
+
+later(function()
+  local MiniPick = require('mini.pick')
+  local MiniExtra = require('mini.extra')
+
+  MiniPick.setup({ window = { config = { border = vim.g.border_style }, prompt_prefix = '{ ', prompt_caret = ' }' } })
+  vim.ui.select = MiniPick.ui_select
+
+
+  vim.keymap.set('n', '<leader><leader>', MiniPick.builtin.buffers, { desc = '[S]earch [B]uffers' })
+  vim.keymap.set('n', '<leader>sd', MiniExtra.pickers.diagnostic, { desc = '[S]earch [D]iagnostics' })
+  vim.keymap.set('n', '<leader>sf', MiniPick.builtin.files, { desc = '[S]earch [F]iles' })
+  vim.keymap.set('n', '<leader>sg', MiniPick.builtin.grep_live, { desc = 'Search by [G]rep' })
+  vim.keymap.set('n', '<leader>sh', MiniPick.builtin.help, { desc = '[S]earch [H]elp' })
+  vim.keymap.set('n', '<leader>sr', MiniPick.builtin.resume, { desc = 'Search Resume' })
+  vim.keymap.set('n', '<leader>sw', MiniPick.builtin.grep, { desc = '[S]earch [W]ord' })
+end)
+
+later(function()
+  local MiniSessions = require('mini.sessions')
+
+  vim.keymap.set('n', '<leader>es', MiniSessions.select, { desc = 'S[e]ssion [S]elect' })
+  vim.keymap.set('n', '<leader>er', function() MiniSessions.read() end, { desc = 'S[e]ssion [R]ead' })
+  vim.keymap.set('n', '<leader>ew', function() MiniSessions.write() end, { desc = 'S[e]ssion [W]rite' })
+  vim.keymap.set('n', '<leader>ed', function() MiniSessions.delete() end, { desc = 'S[e]ssion [D]elete' })
+end)
+
+later(function()
+  local MiniHipatterns = require('mini.hipatterns')
+  MiniHipatterns.setup({
+    highlighters = {
+      fixme     = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
+      hack      = { pattern = '%f[%w]()HACK()%f[%W]', group = 'MiniHipatternsHack' },
+      todo      = { pattern = '%f[%w]()TODO()%f[%W]', group = 'MiniHipatternsTodo' },
+      note      = { pattern = '%f[%w]()NOTE()%f[%W]', group = 'MiniHipatternsNote' },
+
+      hex_color = MiniHipatterns.gen_highlighter.hex_color(),
+    },
+  })
+end)
+-- }}}
+
+-- [[ LSP ]] {{{
+now(function()
+  vim.lsp.enable({ 'luals' }) -- should always be loaded immediately
 end)
 
 later(function()
@@ -105,9 +158,6 @@ later(function()
   require('mini.completion').setup({
     lsp_completion = { source_func = 'omnifunc', auto_setup = false }
   })
-
-  local MiniPick = require('mini.pick')
-  MiniPick.setup({ window = { config = { border = 'single' }, prompt_prefix = '{ ', prompt_caret = ' }' } })
 end)
 
 vim.api.nvim_create_autocmd('LspAttach', {
@@ -115,28 +165,18 @@ vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(ev)
     vim.bo[ev.buf].omnifunc = 'v:lua.MiniCompletion.completefunc_lsp'
 
-    local MiniPick = require('mini.pick')
-    local MiniExtra = require('mini.extra')
-
-
     local opts = { buffer = ev.buf }
 
-    -- LSP
+    -- TODO: conform.nvim?
     vim.keymap.set('n', '<leader>f', function()
       vim.lsp.buf.format({ async = true })
       vim.notify('Formatted')
     end, opts)
-    vim.keymap.set('n', 'K', function() vim.lsp.buf.hover({ border = 'single' }) end, opts)
-    vim.keymap.set('n', '<C-k>', function() vim.lsp.buf.signature_help({ border = 'single' }) end, opts)
+    vim.keymap.set('n', 'K', function() vim.lsp.buf.hover({ border = vim.g.border_style }) end, opts)
+    vim.keymap.set('n', '<C-k>', function() vim.lsp.buf.signature_help({ border = vim.g.border_style }) end, opts)
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-
-    -- Pick/Extra
-    vim.keymap.set('n', '<leader><leader>', MiniPick.builtin.buffers, opts)
-    vim.keymap.set('n', '<leader>sd', MiniExtra.pickers.diagnostic, opts)
-    vim.keymap.set('n', '<leader>sf', MiniPick.builtin.files, opts)
-    vim.keymap.set('n', '<leader>sg', MiniPick.builtin.grep_live, opts)
-    vim.keymap.set('n', '<leader>sw', MiniPick.builtin.grep, opts)
+    -- TODO: more pickers
   end,
 })
 -- }}}
