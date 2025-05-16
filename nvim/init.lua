02082b6da2ef43120fae0d863212965a7193ace9
Aaron Nguyen <37777232+aatng-gh@users.noreply.github.com>
2025-05-15 23:50:15 -0400

    feat: config backdrop+border

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,6 @@
 vim.o.clipboard = 'unnamedplus'
+vim.g.backdrop = 100
+vim.g.border = 'single'
 
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -17,7 +19,15 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 vim.g.mapleader = ' '
-vim.g.maplocalleader = '\\'
+vim.g.maplocalleader = ' '
+
+vim.api.nvim_create_autocmd('ColorScheme', {
+  pattern = '*',
+  callback = function()
+    vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
+  end,
+})
 
 require('lazy').setup({
   spec = {
@@ -31,8 +41,6 @@ require('lazy').setup({
         },
       },
       config = function(_, opts)
-        vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
-        vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
         require('rose-pine').setup(opts)
         vim.cmd.colorscheme('rose-pine')
       end,
@@ -41,7 +49,7 @@ require('lazy').setup({
       'neovim/nvim-lspconfig',
       dependencies = {
         { 'mason-org/mason.nvim', opts = {
-          backdrop = 0,
+          ui = { backdrop = vim.g.backdrop, border = vim.g.border },
         } },
       },
     },
@@ -73,9 +81,80 @@ require('lazy').setup({
         },
       },
     },
+    {
+      'folke/snacks.nvim',
+      opts = {
+        picker = {
+          layout = {
+            layout = {
+              box = 'horizontal',
+              backdrop = vim.g.backdrop,
+              width = 0.8,
+              min_width = 120,
+              height = 0.8,
+              {
+                box = 'vertical',
+                border = vim.g.border,
+                title = '{title} {live} {flags}',
+                { win = 'input', height = 1, border = 'bottom' },
+                { win = 'list', border = 'none' },
+              },
+              { win = 'preview', title = '{preview}', border = vim.g.border, width = 0.5 },
+            },
+          },
+        },
+        explorer = {},
+      },
+      keys = {
+        -- Top Pickers & Explorer
+        {
+          '<leader><space>',
+          function()
+            Snacks.picker.smart()
+          end,
+          desc = 'Smart Find Files',
+        },
+        {
+          '<leader>,',
+          function()
+            Snacks.picker.buffers()
+          end,
+          desc = 'Buffers',
+        },
+        {
+          '<leader>/',
+          function()
+            Snacks.picker.grep()
+          end,
+          desc = 'Grep',
+        },
+        {
+          '<leader>:',
+          function()
+            Snacks.picker.command_history()
+          end,
+          desc = 'Command History',
+        },
+        {
+          '<leader>n',
+          function()
+            Snacks.picker.notifications()
+          end,
+          desc = 'Notification History',
+        },
+        {
+          '<leader>e',
+          function()
+            Snacks.explorer()
+          end,
+          desc = 'File Explorer',
+        },
+      },
+    },
   },
   ui = {
-    backdrop = 0,
+    backdrop = vim.g.backdrop,
+    border = vim.g.border,
   },
   checker = { enabled = true },
 })
