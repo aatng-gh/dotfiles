24a7579e3512089219ccb0d4783dc1fb2e531c8e
Aaron Nguyen <aaron.nguyen@wellhive.com>
2025-06-18 10:31:45 -0400

    feat: improve folding

--- nvim/init.lua
+++ nvim/init.lua
@@ -42,8 +42,8 @@ local function options() -- {{{
   vim.o.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
   vim.o.foldtext = ''
   vim.o.foldlevel = 99
-  vim.o.foldlevelstart = 1
   vim.o.foldnestmax = 4
+  vim.o.foldcolumn = '1'
 
   vim.schedule(function()
     vim.o.clipboard = 'unnamedplus'
