55e628e48d57f9b944ae2dde051320d8dae4334b
Aaron Nguyen <aatng-gh@users.noreply.github.com>
2025-06-20 15:11:49 -0400

    chore: set shiftwidth to 4

--- nvim/init.lua
+++ nvim/init.lua
@@ -46,8 +46,8 @@ local function options() -- {{{
   vim.o.foldcolumn = '1'
 
   vim.o.expandtab = true
-  vim.o.shiftwidth = 2
-  vim.o.tabstop = 2
+  vim.o.shiftwidth = 4
+  vim.o.tabstop = 4
 
   vim.schedule(function()
     vim.o.clipboard = 'unnamedplus'
