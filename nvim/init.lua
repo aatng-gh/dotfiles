0f907a6f2f6daa3c347d35ecefe922232360cee3
Aaron Nguyen <37777232+aatng-gh@users.noreply.github.com>
2026-04-10 18:01:50 -0400

    feat(nvim): rework minimal config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,46 @@
-vim.pack.add({ 'https://github.com/nvim-mini/mini.nvim' })
+-- Plugins
+vim.pack.add({
+  'https://github.com/nvim-mini/mini.nvim',
+  { src = 'https://github.com/nvim-treesitter/nvim-treesitter', version = 'main' },
+})
+
+-- Basics
+require('mini.basics').setup({
+  options = {
+    basic = true,
+    extra_ui = true,
+    win_borders = 'single',
+  },
+  autocommands = {
+    basic = true,
+  },
+})
+
+-- Options
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+vim.o.autocomplete = true
+
+-- Autocommands
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('lsp_completion', { clear = true }),
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if client and client:supports_method('textDocument/completion') then
+      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+    end
+  end,
+})
+
+-- LSP
+vim.lsp.enable({ 'lua_ls', 'vtsls' })
+
+-- Keymaps
+vim.keymap.set('n', '<leader>f', function()
+  vim.lsp.buf.format({ async = true })
+end, { desc = 'Format' })
+
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = 'Definition' })
+vim.keymap.set({ 'n', 'x' }, 'gy', '"+y', { desc = 'Copy to clipboard' })
+vim.keymap.set('n', 'gY', '"+Y', { desc = 'Copy line to clipboard' })
