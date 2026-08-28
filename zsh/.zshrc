7bf839cbb13fe75f5d522c917515007f2d852036
Aaron Nguyen <37777232+aatng-gh@users.noreply.github.com>
2026-08-27 23:23:47 -0400

    Enable personal Zsh overlay

--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,1 +1,2 @@
+zstyle ':dotfiles:load' overlays personal.d
 source "${XDG_CONFIG_HOME:-$HOME/.config}/zsh/zshrc"
---
+++ zsh/personal.d/40-aliases.zsh
@@ -0,0 +1,3 @@
+# dxdiag image mount/unmount
+alias dx-on='diskutil image attach --nobrowse "$HOME/dev/dxdiag/DiagnosticReports.sparsebundle"'
+alias dx-off="diskutil unmount /Volumes/dxdiag"
